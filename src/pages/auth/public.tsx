48314e10943a26b53118376a7dc5a1dd52f7e87f
Xoadra <xijp77@gmail.com>
2019-12-14 01:04:24 -0800

    Established remote markdown parsing via Netlify function

--- src/pages/auth/public.tsx
+++ src/pages/auth/public.tsx
@@ -2,7 +2,12 @@
 
 
 
-import React from 'react'
+import React, { Dispatch, useState, useEffect, useContext } from 'react'
+import axios, { AxiosResponse } from 'axios'
+
+import Document from '../../components/document'
+import GitHubContext from '../../services/githubcontext'
+import { GitHubAuth } from '../../types/githubauth'
 
 
 
@@ -11,11 +16,46 @@ interface Props {
 }
 
 
-export default (props: Props) => (
-	<div>
-		<h3>Public Page</h3>
-		<p>Nothing cool here. Sad!</p>
-	</div>
-)
+export default (props: Props) => {
+	const authenticator: GitHubAuth = useContext(GitHubContext)
+	const [html, setHtml]: [string, Dispatch<any>] = useState('')
+	const query: object = {
+		query: `query($owner: String!, $name: String!, $expression: String!) {
+			repository(owner: $owner, name: $name) {
+				object(expression: $expression) {
+					... on Blob {
+						id
+						oid
+						text
+					}
+				}
+			}
+		}`,
+		variables: {
+			owner: 'Xoadra',
+			name: 'IanPortfolio',
+			expression: 'master:README.md'
+		}
+	}
+	useEffect(() => {
+		if (!html) {
+			(async () => {
+				const url: string = 'https://api.github.com/graphql'
+				const headers: object = { 'Authorization': `Bearer ${authenticator.token}` }
+				const graphql: AxiosResponse = await axios.post(url, query, { headers })
+				const netlify: AxiosResponse = await axios.post('/.netlify/functions/remark', graphql)
+				setHtml(netlify.data.html)
+			})()
+		}
+	}, [html])
+	return (
+		<div>
+			<h3>Public Page</h3>
+			<p>Very public. No privacy whatsoever. Sad!</p>
+			{html ? <Document html={html}/> : <p>Loading...</p>}
+		</div>
+	)
+}
+
 
 
